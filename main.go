45722972121b0f9b1c5b0b6007afdfe74c9ef892
Uchencho <aloziekelechi17@gmail.com>
2020-10-19 11:52:24 +0100

    connecting to mongoDB

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/Uchencho/Account/server"
+)
+
 func main() {
 
+	fmt.Println("It starts here")
+
+	defer func() {
+		ctx := context.Background()
+		if err := server.Client.Disconnect(ctx); err != nil {
+			log.Fatalln("Error in disconnecting", err)
+		}
+		log.Println("Disconnected successfully")
+	}()
+	fmt.Println("It connected")
+
+	fmt.Println("When will this be called?")
 }
---
+++ server/database.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func ConnectDB() *mongo.Client {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal("Error in connecting to db")
+	}
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatalln("Could not ping db with error", err)
+	}
+	log.Println("Connected to MongoDB")
+	return client
+}
+
+var Client = ConnectDB()
